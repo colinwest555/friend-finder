b10f8ffb194a5408d6404daab8ec3eca1148e8ec
colin westmoreland <colinwest555@gmail.com>
2019-04-26 16:25:34 -0500

    almost

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Rick ",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+    scores: [
+      "SA",
+      "SA",
+      "A",
+      "A",
+      "SD",
+      "SD",
+      "A",
+      "A",
+      "A",
+      "A"
+    ]
+  },
+  {
+    name: "Morty",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441",
+    scores: [
+      "D",
+      "SD",
+      "SD",
+      "A",
+      "N",
+      "A",
+      "A",
+      "D",
+      "N",
+      "A"
+    ]
+  },
+  {
+    name: "Jerry",
+    photo: "https://vignette.wikia.nocookie.net/ricksanchez/images/f/f6/Jerry_Smith.jpg/revision/latest?cb=20160609043244",
+    scores: [
+      "A",
+      "D",
+      "D",
+      "D",
+      "SA",
+      "SA",
+      "N",
+      "D",
+      "N",
+      "SD"
+    ]
+  },
+  {
+    name: "Mr Meseeks",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/6/6c/MeeseeksHQ.png/revision/latest?cb=20150930232412",
+    scores: [
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N",
+      "N"
+    ]
+  },
+  {
+    name: "Beth",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.png/revision/latest?cb=20151204220729",
+    scores: [
+      "N",
+      "A",
+      "SA",
+      "D",
+      "A",
+      "SD",
+      "SD",
+      "N",
+      "D",
+      "SA"
+    ]
+  },
+  {
+    name: "Summer",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158",
+    scores: [
+      "D",
+      "A",
+      "SD",
+      "N",
+      "N",
+      "N",
+      "N",
+      "SD",
+      "A",
+      "SD"
+    ]
+  }
+
+];
+
+
+module.exports = friends;
